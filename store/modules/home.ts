285aa76934d091595dd2d5a2f19629b39812ed0c
Pitsanu Limpanachaipornkul <pitsanu.li@kskgroup.com>
2020-12-26 08:56:29 +0700

    first commit

---
+++ store/modules/home.ts
@@ -0,0 +1,22 @@
+// import store from '../index';
+// import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators';
+
+// @Module({
+//     dynamic: true,
+//     name: 'home',
+//     namespaced: true,
+//     preserveState: true
+// })
+// export default class HomeModule extends VuexModule {
+//     private name: string = '';
+
+//     get nameHome() {
+//         return this.name;
+//     }
+
+//     @Mutation
+//     public setName(name: string): void {
+//         this.name = name;
+//     }
+
+// }
---
+++ store/namespace.ts
@@ -0,0 +1,4 @@
+// import { getModule } from 'vuex-module-decorators';
+// import { HomeModule } from './modules';
+
+// export const homeModule = getModule(HomeModule);
